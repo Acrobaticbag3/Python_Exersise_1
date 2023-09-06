93220a28e7726752355e6a10ec26fd8a7f67cd91
Acrobaticbag3 <kevintom214@gmail.com>
2023-09-06 08:38:58 +0200

    Done with g

--- 1DV501/kj223ag_assign1/shortname.py
+++ 1DV501/kj223ag_assign1/shortname.py
@@ -4,5 +4,4 @@ middle_Name = input("Middle name: ")
 last_Name = input("Last name: ")
 
 # abreviated name
-abv_Name = first_Name[0], middle_Name[0], last_Name
-print(abv_Name)
+print(f"abreviated name:{first_Name[0]}. {middle_Name[0]}.", last_Name)
