38f53d98b4eb8c377f6c5b4f9ead58af837115e2
Acrobaticbag3 <kevintom214@gmail.com>
2023-09-06 11:43:19 +0200

    Updated assignment 8 aka change

--- 1DV501/kj223ag_assignment1VG/change.py
+++ 1DV501/kj223ag_assignment1VG/change.py
@@ -1,14 +1,30 @@
 # assignment 8
+# Function
 def check_Amount(change):
-    int_bill_value = {1000, 500, 200, 100, 50, 20}  # array of ints 
+    int_Bill_Value = {1000, 500, 200, 100, 50, 20}  # Bill values 
+    int_Coin_Value = {10, 5, 2, 1}                  # Coin values
 
-    # for every element in arr 
-    for element in int_bill_value:              
-        if change % int_bill_value[element]:    # if change and element are devisable
-            change - element                    #
-            print(change, element)
+    # loop through our bill array and see what values we get
+    for element in int_Bill_Value:
+        try:
+            if change >= element:   # check that change is bigger than or == element value 
+                change -= element
+                print(element, "bills:", 1)
+        except:
+            print("There was an exception!")
+    
+    # loop through our coin array and see what values we get
+    while change != 0:             # Lloop through as long as change != 0 
+        for idex in int_Coin_Value:     
+            try:
+                if change >= idex:  # check that change is bigger than or == elemental value 
+                    change -= idex
+                    print(idex, "Coins:")
+            except:
+                print("There was an exception!")
 
 
+# Main program
 price = int(input("Price: "))
 payment = int(input("Payment: "))
 
