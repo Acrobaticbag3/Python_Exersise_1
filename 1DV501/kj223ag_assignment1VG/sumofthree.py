470ce73d7241f4e6e42e3b5c3453672f75c6bbbd
Acrobaticbag3 <kevintom214@gmail.com>
2023-09-10 11:54:13 +0200

    Update sumofthree.py

--- 1DV501/kj223ag_assignment1VG/sumofthree.py
+++ 1DV501/kj223ag_assignment1VG/sumofthree.py
@@ -1,11 +1,18 @@
 # assignment 7 vg task
 three_Didgit = input("Provide a three digit number: ")
 
-# Try to run code Note: - Does not work with negative numbers
+# try to run code Note: - Does not work with negative numbers
 try:
-    num_01, num_02, num_03 = int(three_Didgit[0]), int(three_Didgit[1]), int(three_Didgit[2])   # get and convert numbers from string to int
-    sum = num_01 + num_02 + num_03
-    print("Sum of the three numbers is:",sum)
+    if len(three_Didgit)  == 3: # if its a three length number then we can assme that its positive
+        num_01, num_02, num_03 = int(three_Didgit[0]), int(three_Didgit[1]), int(three_Didgit[2])   # get and convert numbers from string to int
+        sum = num_01 + num_02 + num_03
+        print("Sum of the three numbers is:", sum)
+    elif len(three_Didgit) == 4 and three_Didgit[0] == '-': # if it has 4 length then we check if the first string character is - aka negative
+        num_01, num_02, num_03 = int(three_Didgit[1]), int(three_Didgit[2]), int(three_Didgit[3])   # get and convert numbers from string to int
+        sum = num_02 + num_03 - num_01  # e.g: -746 -> -7 + 4 + 6 = 3
+        print("Sum of the three numbers is:", sum)
+    else:
+        print("Invalid input.")
 # chatch potential error
-except:
-    print("Exeption occured!")
+except ValueError:
+    print("Exception occurred!")
