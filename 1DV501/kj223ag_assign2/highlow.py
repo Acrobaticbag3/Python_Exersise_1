94638a5bec087a262c38430a7a8147e853b3b488
Acrobaticbag3 <kevintom214@gmail.com>
2023-09-18 16:57:20 +0200

    ksfhksdf

--- 1DV501/kj223ag_assign2/highlow.py
+++ 1DV501/kj223ag_assign2/highlow.py
@@ -2,10 +2,28 @@
 import random
 
 
+# Logic for the guesses
 def Check_Guess(rand, guess):
-    if
+    if rand > guess:
+        print("Higher:")
+    elif rand < guess:
+        print("Lower:")
+    elif rand == guess:
+        print(f"Congrats, you found my number {rand}, amount of times guessed: ", end="")  # noqa: E501
 
 
-rand = random(1, 100)
-guess = int(input("Guess: "))
+# Start of main
+rand = random.randint(1, 100)
+guesses = 0
+guess = -1
 
+# Run for as long as our guess != the computers number.
+while guess != rand:
+    guesses += 1
+    try:
+        guess = int(input(f"Guess {guesses} : "))
+        Check_Guess(rand, guess)
+    except ValueError:
+        print("Provide an intager...")
+
+print(guesses, "- Excellent!")
---
+++ 1DV501/kj223ag_assign2/print_primes.py
@@ -0,0 +1,18 @@
+# assignment 5 Broken - does not work
+def Print_Primes(userInput):
+    number = 0
+    i2 = 0
+
+    while i2 != userInput:
+        for i in range(1, 10):
+            number += 1
+            i2 + 1
+
+            if number % number == 0 and number % 1 == 0:
+                print(number, end="")
+            else:
+                print("dfxcgvbhjn")
+
+
+userInput = int(input("How many primes?"))
+Print_Primes(userInput)
