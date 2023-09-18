f1cc8b541f4e298dcfca42f2d86855c8e61b4799
Acrobaticbag3 <kevintom214@gmail.com>
2023-09-18 20:35:53 +0200

    Done

--- 1DV501/kj223ag_assign2/print_primes.py
+++ 1DV501/kj223ag_assign2/print_primes.py
@@ -1,18 +1,41 @@
 # assignment 5 Broken - does not work
-def Print_Primes(userInput):
-    number = 0
-    i2 = 0
+def is_prime(n):
+    if n <= 1:                  # Primes can only be positive
+        return False
+    elif n <= 3 and n != 1:     # 2,3 are primes
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
 
-    while i2 != userInput:
-        for i in range(1, 10):
-            number += 1
-            i2 + 1
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
 
-            if number % number == 0 and number % 1 == 0:
-                print(number, end="")
+    return True
+
+
+def print_primes(n):
+    count = 0
+    track = 0
+    num = 0
+
+    while count != n:
+        num += 1
+
+        if track == 10:
+            track -= track
+            print()
+
+        else:
+            if is_prime(num):
+                count += 1
+                track += 1
+                print(num, end=' ')
             else:
-                print("dfxcgvbhjn")
+                pass
 
 
-userInput = int(input("How many primes?"))
-Print_Primes(userInput)
+userInput = int(input("How many primes? "))
+print_primes(userInput)
